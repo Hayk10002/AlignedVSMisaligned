203482a8d04819258fa0fc9166f0353ccc191e6e
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-04-14 15:01:08 +0400

    change the order of tests

--- main.cpp
+++ main.cpp
@@ -121,9 +121,9 @@ int main(int argc, char* argv[]) {
 
     std::cout << "Very misaligned pointers calculated." << std::endl;
 
-    test(data, N, very_misalign_a, very_misalign_b, very_misalign_result, very_misalign_bytes);
-    test(data, N, misaligned_a, misaligned_b, misaligned_result, misalign_bytes);
     test(data, N, base_a, base_b, base_result, 32);
+    test(data, N, misaligned_a, misaligned_b, misaligned_result, misalign_bytes);
+    test(data, N, very_misalign_a, very_misalign_b, very_misalign_result, very_misalign_bytes);
 
     delete [] raw_a;
     delete [] raw_b;
